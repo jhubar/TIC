b2536792afe42a6e0d14703d9a15b95a77b60bba
francoislievens <60618100+francoislievens@users.noreply.github.com>
2021-04-20 21:58:50 +0200

    LV

---
+++ projet 2/perso.py
@@ -0,0 +1,15 @@
+
+from LempelZiv import *
+
+
+test = LZ77(l=3)
+
+sample = 'BASILE BAVE DANS SON BAVOIR'
+
+test.encode(sample)
+
+for i in range(0, len(test.prev_size)):
+    print('{} - {} - {} - {}'.format(test.source_word_historique[i],
+                                     test.new_symbol[i],
+                                     test.prev_dist[i],
+                                     test.prev_size[i]))
