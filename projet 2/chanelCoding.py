08c734d2fd485e0ea969c50456ad3e980740032f
jhubar <hubarjulien@gmail.com>
2021-04-20 20:08:51 +0200

    update coding

---
+++ projet 2/chanelCoding.py
@@ -0,0 +1,70 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import wave
+import sys
+import os
+import scipy.io
+from scipy.io.wavfile import read
+import numpy as np
+import matplotlib.pyplot as plt
+import random 
+import re
+
+from utils import *
+
+
+class coding():
+    def __init__(self):
+        self.rate, self.data = load_wav()
+        self.sampleRate = None
+        
+        self.duration = len(self.data)/self.rate
+        self.bin_data = ""
+        self.channelized_data = ""
+        self.num_of_bits = 8
+        self.probability = 0.01
+        self.decoded_data = []
+        
+    
+    def binarize_data(self):
+        for i in self.data: 
+            self.bin_data += binarize(i,int(self.num_of_bits))
+        return str(self.bin_data)
+    
+    def encode_sound_signal(self):
+        self.num_of_bits = np.log2(np.max(self.data) + 1)
+        print("The number of bits to encode the sound signal is: "+ str(self.num_of_bits))
+        self.binarize_data()
+    
+    def simulate_channel(self):
+        for i in self.data:
+            uniform = random.uniform(0, 1)
+            if uniform <= self.probability:
+                if i != 1: 
+                    self.channelized_data = "1"
+                else:
+                    self.channelized_data = "0"
+            else:
+                self.channelized_data += str(i)
+
+
+    def simulate_and_decode(self):
+        self.simulate_channel()
+        self.channelized_data = re.findall('.{1,8}', self.channelized_data)
+        for i in self.channelized_data:
+            self.channelized_data.append(bin_to_dec(i),dtype=np.uint8)
+        # self.decoded_data = np.array([bin_to_dec(val) for val in self.channelized_data],dtype=np.uint8)
+
+    def plot_sound_signal(self, data = None):
+        if data is None:
+            self.time = np.arange(0,self.data,1/self.rate) #time vector
+            plt.plot(self.time,self.data)
+        else:
+            self.time = np.arange(0,data,1/self.rate) #time vector
+            plt.plot(self.time,data)
+        plt.xlabel('Time [s]')
+        plt.ylabel('Amplitude')
+        plt.title("wav")
+        plt.savefig("fig/wav.pdf")
+        plt.show()
+        plt.close()
--- projet 2/main.py
+++ projet 2/main.py
@@ -1,23 +1,33 @@
 
 from huffman import *
-
+from chanelCoding import *
 
 
 if __name__ == "__main__":
 
 
-    P_S =  'BCAADDDCCACACAC'
-    huffman = Huffman(P_S)
-    huffman.run_hoffman_code()
-    huffman.print_huffman_code()
+    # P_S =  'BCAADDDCCACACAC'
+    # huffman = Huffman(P_S)
+    # huffman.run_hoffman_code()
+    # huffman.print_huffman_code()
     
 
-    P_S = [ 8, 3, 3, 3, 3]
-    huffman = Huffman(P_S)
-    huffman.run_hoffman_code()
-    huffman.print_huffman_code()
+    # P_S = [ 8, 3, 3, 3, 3]
+    # huffman = Huffman(P_S)
+    # huffman.run_hoffman_code()
+    # huffman.print_huffman_code()
 
-    P_S = [0.05, 0.10, 0.15, 0.15, 0.2, 0.35]
-    huffman = Huffman(P_S)
-    huffman.run_hoffman_code()
-    huffman.print_huffman_code()
+    # P_S = [0.05, 0.10, 0.15, 0.15, 0.2, 0.35]
+    # huffman = Huffman(P_S)
+    # huffman.run_hoffman_code()
+    # huffman.print_huffman_code()
+
+    sound_path = "suppl/sound.wav"
+    
+    coding = coding()
+    
+    # coding.plot_sound_signal()
+    coding.encode_sound_signal()
+    coding.simulate_and_decode()
+    coding.plot_sound_signal(coding.decoded_data)
+
