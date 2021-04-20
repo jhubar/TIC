74ccbd7d8f73f16eae30ce19b253ffa2c19b210d
jhubar <hubarjulien@gmail.com>
2021-04-20 16:00:12 +0200

    update

--- projet 2/main.py
+++ projet 2/main.py
@@ -10,14 +10,14 @@ if __name__ == "__main__":
     huffman = Huffman(P_S)
     huffman.run_hoffman_code()
     huffman.print_huffman_code()
-    # huffman.print_huffman_code()
+    
 
-    # P_S = [ 8, 3, 3, 3, 3]
+    P_S = [ 8, 3, 3, 3, 3]
     huffman = Huffman(P_S)
     huffman.run_hoffman_code()
     huffman.print_huffman_code()
 
-    # P_S = [0.05, 0.10, 0.15, 0.15, 0.2, 0.35]
+    P_S = [0.05, 0.10, 0.15, 0.15, 0.2, 0.35]
     huffman = Huffman(P_S)
     huffman.run_hoffman_code()
     huffman.print_huffman_code()
