3e195cd870d21aa63315c5ab88ab8ed93cf104cd
jhubar <hubarjulien@gmail.com>
2021-04-24 17:46:04 +0200

    up

--- projet 2/main.py
+++ projet 2/main.py
@@ -2,6 +2,27 @@
 from huffman import *
 from chanelCoding import *
 
+def run_part_1():
+    P_S =  'BCAADDDCCACACAC'
+    huffman = Huffman(P_S)
+    huffman.run_hoffman_code()
+    huffman.print_huffman_code()
+
+
+    P_S = [ 8, 3, 3, 3, 3]
+    huffman = Huffman(P_S)
+    huffman.run_hoffman_code()
+    huffman.print_huffman_code()
+
+    P_S = [0.05, 0.10, 0.15, 0.15, 0.2, 0.35]
+    huffman = Huffman(P_S)
+    huffman.run_hoffman_code()
+    huffman.print_huffman_code()
+
+
+    coding.encode_sound_signal()
+    coding.simulate_and_decode()
+    coding.plot_sound_signal(coding.decoded_data)
 
 def run_part_2():
     sound_path = "suppl/sound.wav"
@@ -18,26 +39,5 @@ def run_part_2():
     coding.simulate_and_decode(input_data = coding.hamming_code, name="hamming", decode = True)
 
 if __name__ == "__main__":
-
+    # run_part_1()
     run_part_2()
-
-    # P_S =  'BCAADDDCCACACAC'
-    # huffman = Huffman(P_S)
-    # huffman.run_hoffman_code()
-    # huffman.print_huffman_code()
-
-
-    # P_S = [ 8, 3, 3, 3, 3]
-    # huffman = Huffman(P_S)
-    # huffman.run_hoffman_code()
-    # huffman.print_huffman_code()
-
-    # P_S = [0.05, 0.10, 0.15, 0.15, 0.2, 0.35]
-    # huffman = Huffman(P_S)
-    # huffman.run_hoffman_code()
-    # huffman.print_huffman_code()
-
-
-    # coding.encode_sound_signal()
-    # coding.simulate_and_decode()
-    # coding.plot_sound_signal(coding.decoded_data)
